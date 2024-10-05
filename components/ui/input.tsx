915d4004b27a42a96d84ef30dcf2f158b1668d5e
Frank Borja <fborjaz@unemi.edu.ec>
2024-10-05 11:59:13 -0500

    fix(Errores): :rotating_light: Correcion estaba causando el error de ESLint

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,13 +1,12 @@
+// components/ui/input.tsx
+
 "use client";
 
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
